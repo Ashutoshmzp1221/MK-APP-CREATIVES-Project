422fbc92dfd02f38033661ce7449dd44d1981b46
Ashutosh Dwivedi <ashutoshmzp1221@gmail.com>
2025-06-14 13:34:00 +0530

    Setup the service layer

---
+++ src/services/upload-service.js
@@ -0,0 +1,47 @@
+const UploadRepository = require('../repositories/upload-repository');
+const JobQueue = require('../config/jobQueue');
+class UploadService {
+    constructor() {
+        this.uploadRepository = new UploadRepository();
+        this.jobQueue = new JobQueue();
+    }
+
+    async handleUpload(file, metaDataJson) {
+        try {
+            const metadata = JSON.parse(metadataJson);
+            const upload = await this.uploadRepository.create({
+                filename: file.originalname,
+                metadata,
+                status: 'processing',
+                progress: 0,
+                stored_path: file.path
+            });
+            this.jobQueue.add(upload.id);
+            return upload;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw { error };
+        }
+    }
+
+    async getStatus(id) {
+        try {
+            const status = await this.uploadRepository.findById(id);
+            return status;
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw { error };
+        }
+    }
+
+    async listFiles() {
+        try {
+            const lists = await this.uploadRepository.findAll();
+        } catch (error) {
+            console.log('Something went wrong in the service layer');
+            throw { error };
+        }
+    }
+}
+
+module.exports = UploadService;
